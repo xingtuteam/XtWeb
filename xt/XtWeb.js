8c6ad4da8052def1cfc6a0fa2c33bd2d5583a3c0
xingtu <96755323+xingtuteam@users.noreply.github.com>
2023-09-25 19:14:31 +0800

    Update XtWeb.js
    补丁

--- xt/XtWeb.js
+++ xt/XtWeb.js
@@ -8,7 +8,7 @@ export default class XtWeb {
     static get address() {
         return {
             toXtAddress(address) {
-                if (!utils.isHex(address)&&Number(address.length) === 42 )
+                if (!utils.isHex(address)&&!Number(String(address).length) === 42 )
                     return address;
                 return utils.crypto.getBase58CheckAddress(
                     utils.code.hexStr2byteArray(address.replace(/^0x/, ADDRESS_PREFIX))
@@ -29,7 +29,7 @@ export default class XtWeb {
                 }
             },
             addressToHex(value) {
-                if (Number(value.length) === 35 && String(value.slice(0, 2)) === 'X1')
+                if (Number(value.length) === 35 && String(String(value).slice(0, 2)) === 'X1')
                     return this.addressHexString(value).replace(ADDRESS_PREFIX_REGEX, '0x');
                 return value;
 
